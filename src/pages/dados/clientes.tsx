54a3fd6be30fee9ccd940d575048eb5b6ab6acc1
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-03-26 03:12:14 -0300

    requisições feitas na api funcionando

--- src/pages/dados/clientes.tsx
+++ src/pages/dados/clientes.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+
 export default function Clientes(){
+    const [clientes, setClientes] = useState([])
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/clientes')
+            .then(response => response.json())
+            .then(clientes => {
+                setClientes(clientes)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
     return (
         <div>
-            Clientes
+            {
+                clientes.map(cliente => (
+                    <div key={cliente.id}>{cliente.nome}</div>
+                ))
+            }
         </div>
     )
 }
--- src/pages/formularios/cadastro.tsx
+++ src/pages/formularios/cadastro.tsx
@@ -1,7 +1,7 @@
 export default function CadastroCliente(){
     return (
         <div>
-            
+            Aqui ficará o formulário
         </div>
     )
 }
