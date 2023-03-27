34df95be39ad812f2881188bd9ce5832eed7650d
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-03-27 17:17:15 -0300

    inicio da estilização do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,11 @@ import styles from '../styles/Footer.module.css'
 export default function Footer(){
     return (
         <div className={styles.footer}>
-            Aqui ficará o rodapé
+            <label>&shy; Eduardo - 2023</label>
+            <label>
+                <a href="#">GitHub</a>
+                <a href="#">Linkedln</a>
+            </label>
         </div>
     )
 }
