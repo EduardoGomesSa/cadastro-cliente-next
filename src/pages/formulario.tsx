3671e487226c6d6819832a1226bdf16b3a4337f6
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-03-28 23:32:24 -0300

    criação do module css do formulario

--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -1,10 +1,11 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Cadastro from "../formularios/cadastro";
+import styles from "../styles/Formulario.module.css"
 
 export default function CadastroCliente(){
     return (
-        <div>
+        <div className={styles.formulario}>
             <Header />
             <Cadastro />
             <Footer />
