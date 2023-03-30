55148f95e5a8aef6e0bc11bb0ac4087bf5150007
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-03-29 23:27:17 -0300

    inicio da estilização do css da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Footer from '../components/footer'
+import Header from '../components/header'
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      Aqui será a Home
+    <div className={styles.home}>
+      <Header />
+      <div>Home</div>
+      <Footer />
     </div>
   )
 }
