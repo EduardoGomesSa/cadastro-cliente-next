824d193d9c6954f7b4109777fec7fa48d3b40d15
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-03-31 00:31:59 -0300

    substituição das logo do github e linkedln e edição do tamanho no css do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer(){
         <div className={styles.footer}>
             <label>&shy; Eduardo - 2023</label>
             <label>
-                <a href="#"><img src="./images/github.svg" alt="" /></a>
-                <a href="#"><img src="./images/linkedin.svg" alt="" /></a>
+                <a href="#"><img src="./images/github.png" alt="" className={styles.img} /></a>
+                <a href="#"><img src="./images/linkedln.png" alt="" className={styles.img} /></a>
             </label>
         </div>
     )
