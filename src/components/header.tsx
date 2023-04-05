7615e95d0a9d904f2e6dc8442e54df8f71f20800
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-04 23:26:35 -0300

    inicio da estilização da logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,9 @@ import styles from '../styles/Header.module.css'
 export default function Header(){
     return (
         <div className={styles.header}>
+            <div className={styles.logo}>
+                logo
+            </div>
             Aqui Ficará o cabeçalho
         </div>
     )
