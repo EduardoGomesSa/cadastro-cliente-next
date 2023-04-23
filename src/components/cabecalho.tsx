66633f7e75d91ee3fc9f9cc7da4271b2bbc665e9
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-22 22:12:24 -0300

    preenchimento de toda a área disponivel do index

--- src/components/cabecalho.tsx
+++ src/components/cabecalho.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/Cabecalho.module.css'
 
 export default function Cabecalho(){
     return (
-        <div className={styles.cabecalho}>
+        <div className={styles.cabecalho} id={styles.root}>
             Aqui ficará as informações do cabeçalho CARALHOOOOOO
         </div>
     )
