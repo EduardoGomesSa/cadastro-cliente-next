d5365d02a435d88457df828081881a334ac1b38c
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-24 23:55:24 -0300

    definição de 50% do espaço para a imagem

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/Aside.module.css'
+
 export default function Aside(){
     return (
-        <div>
+        <div className={styles.aside}>
             Aqui ficará uma imagem ilustrativa
         </div>
     )
