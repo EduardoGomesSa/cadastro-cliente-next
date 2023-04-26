72e42b6d0b613aed6e5f9bd1d962e394d2820430
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-25 23:16:15 -0300

    criação do component de address, que armazerá e apresentará os links

---
+++ src/components/address.tsx
@@ -0,0 +1,7 @@
+export default function Address(){
+    return (
+        <div>
+            Enderecos aqui
+        </div>
+    )
+}
--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,8 +1,11 @@
+import Address from "./address";
+
 export default function Content(){
     return (
         <div>
             <a href="dados/clientes">Ver clientes</a><br />
             <a href="formulario">Adicionar cliente</a>
+            <Address />
         </div>
     )
 }
