b324311c21aef2f94ade806bb8dad8b964812479
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-29 22:08:18 -0300

    criação do arquivo css de content

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,8 +1,9 @@
 import Address from "./address";
+import styles from '../styles/Content.module.css'
 
 export default function Content(){
     return (
-        <div>
+        <div className={styles.content}>
             <Address link="dados/clientes" text="Ver clientes" />
             <Address link="formulario" text="Adicionar cliente" />
         </div>
