952a90b0f925f1de01feb062e7f05ca9c32e3f84
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-03 23:40:46 -0300

    adicionando uma imagem lateral fixa

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Aside.module.css'
 export default function Aside(){
     return (
         <div className={styles.aside}>
-            <img src="https://source.unsplash.com/random" alt="Imagem aleatória da tela inicial" className={styles.img} />
+            <img src="https://fazendodinheiroemcasa.com/wp-content/uploads/2020/05/354-3548901_happy-client-png-human-resource-management-animation.png" alt="Imagem aleatória da tela inicial" className={styles.img} />
         </div>
     )
 }
