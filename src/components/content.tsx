2b3662cfe937cfce23d42dfe5a425454512762a1
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-09 23:52:27 -0300

    adição de titulo na tela principal

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Content.module.css'
 export default function Content(){
     return (
         <div className={styles.content}>
+            <h1>Tela Principal</h1>
             <Address link="dados/clientes" text="Ver clientes" />
             <Address link="formulario" text="Adicionar cliente" />
         </div>
