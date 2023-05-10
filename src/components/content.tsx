af4a2ebb7bcaac52c2335f3380bc282fab8cc5c1
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-09 23:58:35 -0300

    margin do titulo mais afastada

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Content.module.css'
 export default function Content(){
     return (
         <div className={styles.content}>
-            <h1>Tela Principal</h1>
+            <h1 className={styles.titulo}>Tela Principal</h1>
             <Address link="dados/clientes" text="Ver clientes" />
             <Address link="formulario" text="Adicionar cliente" />
         </div>
